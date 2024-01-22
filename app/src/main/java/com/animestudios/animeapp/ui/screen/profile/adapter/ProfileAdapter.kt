63e14685078653ecc5430a695f8f7b7e74d1506c
professorDeveloper <azamovhud007@outlook.com>
2024-01-22 23:40:49 +0500

    Add ProfileAdapter For Category

--- app/src/main/java/com/animestudios/animeapp/ui/screen/profile/adapter/ProfileAdapter.kt
+++ app/src/main/java/com/animestudios/animeapp/ui/screen/profile/adapter/ProfileAdapter.kt
@@ -1,33 +1,47 @@
 package com.animestudios.animeapp.ui.screen.profile.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.animestudios.animeapp.databinding.ProfileCategoryItemBinding
 import com.animestudios.animeapp.model.ProfileCategoryModel
 
 class ProfileAdapter : RecyclerView.Adapter<ProfileAdapter.ProfileVh>() {
-    private val list =ArrayList<ProfileCategoryModel>()
+    private val list = ArrayList<ProfileCategoryModel>()
 
 
     inner class ProfileVh(val binding: ProfileCategoryItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun onBind() {
+        fun onBind(data: ProfileCategoryModel) {
             binding.apply {
-                binding.shapeableImageView.setImageResource()
+                binding.shapeableImageView.setImageResource(data.image)
+                binding.textView5.text = data.title
+                binding.description.text = data.description
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileVh {
-        TODO("Not yet implemented")
+        return ProfileVh(
+            ProfileCategoryItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    fun submitList(newList: ArrayList<ProfileCategoryModel>) {
+        list.clear()
+        list.addAll(newList)
     }
 
     override fun onBindViewHolder(holder: ProfileVh, position: Int) {
-        TODO("Not yet implemented")
+        holder.onBind(list.get(position))
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 
 }
