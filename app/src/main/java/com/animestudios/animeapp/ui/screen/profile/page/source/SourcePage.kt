3b6f45af0df7dd550e3cbbf337bb565cd7624470
professorDeveloper <azamovhud007@outlook.com>
2024-09-27 02:28:06 +0500

    source selector ui added

---
+++ app/src/main/java/com/animestudios/animeapp/ui/screen/profile/page/source/SourcePage.kt
@@ -0,0 +1,18 @@
+package com.animestudios.animeapp.ui.screen.profile.page.source
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.animestudios.animeapp.R
+import com.animestudios.animeapp.databinding.SourcePageBinding
+
+class SourcePage :Fragment() {
+
+    private  var _binding:SourcePageBinding?=null
+    private val binding get() = _binding!!
+    //
+
+
+}
